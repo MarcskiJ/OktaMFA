694ebbf18d840f4413135e6d77d544634717a515
MarcskiJ <marc@radtothepowerofsick.com>
2016-07-07 15:59:29 -0700

    Update InstallMFA.ps1

--- InstallMFA.ps1
+++ InstallMFA.ps1
@@ -1,14 +1,14 @@
 Stop-Service adfssrv -Force
 $path = Read-Host "Enter Directory containing install files (eg C:\Admin)"
-Copy-Item C:\Admin\OktaMFA-ADFS.dll.config -Destination C:\Windows\ADFS\OktaMFA-ADFS.dll.config -Force
-Set-Location C:\Admin
+Copy-Item $path + "\OktaMFA-ADFS.dll.config -Destination C:\Windows\ADFS\OktaMFA-ADFS.dll.config" -Force
+Set-Location $path
 [System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
 $newton = New-Object System.EnterpriseServices.Internal.Publish
-$newton.GacInstall("C:\Admin\Newtonsoft.Json.dll")
+$newton.GacInstall($path + "\Newtonsoft.Json.dll")
 $publish = New-Object System.EnterpriseServices.Internal.Publish
-$publish.GacInstall("C:\Admin\OktaMFA-ADFS.dll")
+$publish.GacInstall($path + "\OktaMFA-ADFS.dll")
 $publish1 = New-Object System.EnterpriseServices.Internal.Publish
-$publish1.GacInstall("C:\Admin\OktaMFASMS-ADFS.dll")
+$publish1.GacInstall($path + "\OktaMFASMS-ADFS.dll")
 $typeName = "OktaMFASMS_ADFS.AuthenticationAdapter, OktaMFASMS-ADFS, Version=1.0.0.0, Culture=neutral, PublicKeyToken=2b0ec32601026327"
 $typeName1 = "OktaMFA_ADFS.AuthenticationAdapter, OktaMFA-ADFS, Version=1.0.0.0, Culture=neutral, PublicKeyToken=2b0ec32601026327"
 Start-Service adfssrv
